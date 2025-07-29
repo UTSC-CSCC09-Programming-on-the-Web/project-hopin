40d0784618e7ae61206cea93e5e7211fcb470060
Jane Low <janelow330@gmail.com>
2025-07-29 15:14:14 -0400

    hotfix: fixed frontend for sign-up vs sign-in; google oauth also works for all users now

--- frontend/src/app/signup/layout.tsx
+++ frontend/src/app/signup/layout.tsx
@@ -3,12 +3,9 @@ import HopinLogo from "../../components/hopin-logo";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="min-h-screen flex flex-col">
-      <div className="flex items-center p-4 gap-8">
-        <HopinLogo />
-      </div>
-      {/* <div className="flex flex-row gap-8 p-20 pt-0"> */}
-        <div className="flex-grow">{children}</div>
-      {/* </div> */}
+      <HopinLogo />
+      {children}
+   
     </main>
   );
 }
